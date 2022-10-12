2d38184a1c20c451464c02b087e77388e97dd960
PushyamiKaveti <kaveti.p@husky.neu.edu>
2022-10-12 15:48:16 -0400

    added launch file to load tf frame ids and rqt reconfigure

--- src/robot_static_tf_publisher.cpp
+++ src/robot_static_tf_publisher.cpp
@@ -3,12 +3,13 @@
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <tf2/LinearMath/Quaternion.h>
-
+#include <string>
 
 #include <dynamic_reconfigure/server.h>
 #include <robot_desc/dynamic_paramsConfig.h>
 double tx, ty, tz, rx, ry, rz;
 
+using namespace std;
 void callback(robot_desc::dynamic_paramsConfig &config, uint32_t level) {
   ROS_INFO("Reconfigure Request: %f %f %f %f %f %f", 
             config.rx, config.ry, config.rz, config.tx, config.ty, config.tz);
@@ -31,7 +32,15 @@ int main(int argc, char **argv) {
 
   ros::Rate ros_rate(50);
   tf2_ros::TransformBroadcaster tf_bcast{};
- 
+  ROS_INFO_STREAM("*** PARAMETER SETTINGS ***");
+  string parent, child;
+  private_node.getParam("lidar_frame", parent);
+    
+  ROS_INFO_STREAM("  parent frame: " << parent);
+
+  private_node.getParam("cam_frame", child);
+    
+  ROS_INFO_STREAM("  child frame: " << child);
   while(ros::ok()){
      
  // ROS_INFO("pUblishing static transform");
@@ -39,8 +48,8 @@ int main(int argc, char **argv) {
      geometry_msgs::TransformStamped transformStamped;
   
      transformStamped.header.stamp = ros::Time::now();
-     transformStamped.header.frame_id = "os1_lidar";
-     transformStamped.child_frame_id = "cam_2_optical_frame";
+     transformStamped.header.frame_id = parent;     //"os1_lidar";
+     transformStamped.child_frame_id =  child;            //"cam_2_optical_frame";
      transformStamped.transform.translation.x = tx;
      transformStamped.transform.translation.y = ty;
      transformStamped.transform.translation.z = tz;
